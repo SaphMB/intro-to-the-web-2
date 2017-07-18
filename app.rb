b1c79e56d26593bce0163b53fe280055de15df8f
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-18 13:04:03 +0100

    Add /named-cat route

--- app.rb
+++ app.rb
@@ -6,7 +6,13 @@ get '/' do
   'hello cat lovers!'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
